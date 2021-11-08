3f5bfbdfc0b05530f53982bac0cf1910a7918120
Vitaliy Topoluk <v.topoluk@gmail.com>
2021-11-08 14:23:45 +0200

    Drop dev Logger

--- src/Viewer.php
+++ src/Viewer.php
@@ -30,13 +30,11 @@ class Viewer
             false,
             'html',
         );
-        Logger::telegram('Send ' . $message->getMessageId());
         $_SESSION['messageId'] = $message->getMessageId();
     }
 
     private static function update(int $id, Client $bot, string $banner, string $caption, $inlineKeyboardMarkup = null, $replyKeyboardMarkup = null)
     {
-        Logger::telegram('Update ' . $_SESSION['messageId']);
         try {
             /** @var bool|Message $message */
             $message = $bot->editMessageMedia(
